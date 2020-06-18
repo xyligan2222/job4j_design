ee16d82f2e4bfdfb3be5526c79e7ef18d18e95c5
xyligan2222 <xyligan2222@yandex.ru>
2020-06-18 13:29:24 +0300

    Load structure

---
+++ chapter_001/src/main/java/ru/job4j/design/Test.java
@@ -0,0 +1,9 @@
+package ru.job4j.design;
+
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("Hello Vasya!!!");
+        System.out.println("Hello Vasya!!!");
+        System.out.println("Hello Vasya!!!");
+    }
+}
