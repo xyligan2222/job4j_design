1f7b07476781ebe95028299645b0681aecaa1b77
xyligan2222 <xyligan2222@yandex.ru>
2020-07-02 17:50:10 +0300

    Test

--- chapter_001/src/main/java/ru/job4j/design/collection/SimpleStack.java
+++ chapter_001/src/main/java/ru/job4j/design/collection/SimpleStack.java
@@ -6,7 +6,7 @@ import java.util.Iterator;
 import java.util.NoSuchElementException;
 
 public class SimpleStack<T> {
-    private ForwardLinked<T> linked = new ForwardLinked<T>();
+    /*private ForwardLinked<T> linked = new ForwardLinked<T>();
 
     public T pop() {
         T rsl = null;
@@ -21,5 +21,5 @@ public class SimpleStack<T> {
 
     public void push(T value) {
         linked.add(value);
-    }
+    }*/
 }
