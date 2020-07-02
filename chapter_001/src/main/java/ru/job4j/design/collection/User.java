2830711d66d8e14dbc42ade3dbc263fa3545a621
xyligan2222 <xyligan2222@yandex.ru>
2020-07-02 11:48:21 +0300

    1. Создать модель User [#307955]

---
+++ chapter_001/src/main/java/ru/job4j/design/collection/User.java
@@ -0,0 +1,51 @@
+package ru.job4j.design.collection;
+
+import java.util.Calendar;
+
+public class User {
+    private String name;
+    private int children;
+    private Calendar birthday;
+
+    public User(String name, int children, Calendar birthday) {
+        this.name = name;
+        this.children = children;
+        this.birthday = birthday;
+    }
+
+    public User(String name, int children) {
+        this.name = name;
+        this.children = children;
+    }
+
+    public User(String name) {
+        this.name = name;
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getChildren() {
+        return children;
+    }
+
+    public void setChildren(int children) {
+        this.children = children;
+    }
+
+    public Calendar getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(Calendar birthday) {
+        this.birthday = birthday;
+    }
+}
