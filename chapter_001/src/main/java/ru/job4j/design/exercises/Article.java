b84229b8fd5642c400009787458cb06249e962af
xyligan2222 <xyligan2222@yandex.ru>
2020-07-10 14:27:42 +0300

    1. Генератор текста.

---
+++ chapter_001/src/main/java/ru/job4j/design/exercises/Article.java
@@ -0,0 +1,28 @@
+package ru.job4j.design.exercises;
+
+
+
+
+import java.util.HashSet;
+import java.util.Map;
+
+public class Article {
+    public static boolean generateBy(String origin, String line) {
+        boolean rsl = true;
+        HashSet<String> mapOrigin = new HashSet<>();
+        HashSet<String> mapLine = new HashSet<>();
+        String[] strAfterOrigin = origin.replaceAll("\\p{Punct}", "").toLowerCase().split(" ");
+        for (int i = 0; i < strAfterOrigin.length; i++) {
+            mapOrigin.add(strAfterOrigin[i]);
+        }
+        String[] strAfterLine = line.replaceAll("\\p{Punct}", "").toLowerCase().split(" ");
+        for (int i = 0; i < strAfterLine.length; i++) {
+            mapLine.add(strAfterLine[i]);
+        }
+        if (!mapOrigin.containsAll(mapLine)) {
+            rsl = false;
+        }
+
+        return rsl;
+    }
+}
