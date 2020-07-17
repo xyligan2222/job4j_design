87a2c152c66dc0627a77dc55701efbf6db1858f6
xyligan2222 <xyligan2222@yandex.ru>
2020-07-17 12:47:08 +0300

    4.0. File [#307978]

---
+++ chapter_002/src/main/java/ru/job4j/design/io/Dir.java
@@ -0,0 +1,21 @@
+package ru.job4j.design.io;
+
+import java.io.File;
+
+public class Dir {
+    public static void main(String[] args) {
+        File file = new File("c:\\projects");
+        if (!file.exists()) {
+            throw new IllegalArgumentException(String.format("Not exist %s", file.getAbsoluteFile()));
+        }
+        if (!file.isDirectory()) {
+            throw new IllegalArgumentException(String.format("Not directory %s", file.getAbsoluteFile()));
+        }
+        System.out.println(String.format("size : %s", file.getTotalSpace()));
+        for (File subfile : file.listFiles()) {
+            System.out.println(subfile.getAbsoluteFile().getName() + " size "
+                    + subfile.getAbsoluteFile().getTotalSpace() / (int) Math.pow(2, 30) + " Gb");
+        }
+    }
+}
+
