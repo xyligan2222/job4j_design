9d347991a6a6de41e771a6eda5b7ede20193f0f3
xyligan2222 <xyligan2222@yandex.ru>
2020-07-20 13:06:17 +0300

    5.1. Именованные аргументы [#307979]

---
+++ chapter_002/src/main/java/ru/job4j/design/io/ArgsName.java
@@ -0,0 +1,37 @@
+package ru.job4j.design.io;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class ArgsName {
+
+    private final Map<String, String> values = new HashMap<>();
+
+    public String get(String key) {
+        return values.get(key);
+    }
+
+    private void parse(String[] args) {
+        if (args.length == 0) {
+            throw new IllegalArgumentException("parameters do not exist");
+        }
+        for (String string : args) {
+            String[] arrayString = string.split("=");
+            values.put(arrayString[0].replace("-", ""), arrayString[1]);
+        }
+    }
+
+    public static ArgsName of(String[] args) {
+        ArgsName names = new ArgsName();
+        names.parse(args);
+        return names;
+    }
+
+    public static void main(String[] args) {
+        ArgsName jvm = ArgsName.of(new String[] {"-Xmx=512", "-encoding=UTF-8"});
+        System.out.println(jvm.get("Xmx"));
+
+        ArgsName zip = ArgsName.of(new String[] {"-out=project.zip", "-encoding=UTF-8"});
+        System.out.println(zip.get("out"));
+    }
+}
