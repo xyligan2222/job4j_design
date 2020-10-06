89069bc1168793511de26d9e8b1614a5e67d8600
xyligan2222 <xyligan2222@yandex.ru>
2020-10-06 12:23:17 +0300

    1. Схема прав пользователи и роли. [#307906]

--- chapter_006/src/main/java/ru/job4j/concurrent/Wget.java
+++ chapter_006/src/main/java/ru/job4j/concurrent/Wget.java
@@ -1,20 +1,25 @@
 package ru.job4j.concurrent;
 
 public class Wget {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         Thread thread = new Thread(
                 () -> {
                     for (int i = 0; i < 100 ; i++) {
                         System.out.print("\rLoading : " + i + "%");
                         try {
-                            Thread.sleep(1000);
+                            Thread.sleep(100);
                         } catch (InterruptedException e) {
-                            e.printStackTrace();
+                            //e.printStackTrace();
+                            Thread.currentThread().interrupt();
+                            System.out.println();
+                            System.out.println(Thread.currentThread().isInterrupted());
+                            System.out.println(Thread.currentThread().getState());
                         }
                     }
                 }
         );
         thread.start();
-        System.out.println("Main");
+        thread.sleep(1000);
+        thread.interrupt();
     }
 }
